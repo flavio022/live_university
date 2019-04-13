94453ceaa3a052b069292a627940ec589615055f
flavio022 <flavio.danilo022@gmail.com>
2019-04-13 12:31:26 -0300

    addind pagination

--- bin/src.js
+++ bin/src.js
@@ -5,3 +5,10 @@ $(document).ready(function(){
       });
     });
   });
+
+  $(document).ready(function () {
+    $('#dtBasicExample').DataTable();
+    $('.dataTables_length').addClass('bs-select');
+  });
+
+  
--- routes/index-route.js
+++ routes/index-route.js
@@ -4,5 +4,8 @@ router.get('/',function(req,res){
     res.render("index.ejs");
 });
 
+router.get('/pagi',function(req,res){
+    res.render("pagination.ejs");
+});
 
 module.exports = router;
